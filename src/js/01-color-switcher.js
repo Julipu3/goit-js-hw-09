387341688ff47f5ba38fb75c3d033e851f789a12
Julipu3 <julia.pim55@gmail.com>
2023-08-25 20:16:21 +0300

    changed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-
+const startBtn = document.querySelector('[data-start]'); 
+        const stopBtn = document.querySelector('[data-stop]'); 
+        let timerId = null; 
+ 
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+          }
+ 
+        startBtn.addEventListener("click", () => { 
+            if (!timerId) { 
+                startBtn.disabled = true; 
+                stopBtn.disabled = false; 
+                timerId = setInterval(() => { 
+                    document.body.style.backgroundColor = getRandomHexColor(); 
+                }, 1000); 
+            } 
+        }); 
+ 
+        stopBtn.addEventListener("click", () => { 
+            if (timerId) { 
+                startBtn.disabled = false; 
+                stopBtn.disabled = true; 
+                clearInterval(timerId); 
+                timerId = null; 
+            } 
+        });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix'; 
+ 
+const form = document.querySelector('.form'); 
+const delayInput = document.querySelector('input[name=delay]'); 
+const stepInput = document.querySelector('input[name=step]'); 
+const amountInput = document.querySelector('input[name=amount]'); 
+ 
+form.addEventListener('submit', onSubmit); 
+ 
+function onSubmit(e) { 
+  e.preventDefault(); 
+ 
+  let delay = Number(delayInput.value); 
+  let step = Number(stepInput.value); 
+  let amount = Number(amountInput.value); 
+  let position = 0; 
+ 
+ 
+for (let i = 1; i<= amount; i+= 1) { 
+  position = i; 
+  createPromise(position, delay) 
+  .then(({position, delay}) => { 
+    Notiflix.Notify.success(`Fulfilled promise ${position} is ${delay}ms`); 
+  }) 
+  .catch(({position, delay}) => { 
+    Notiflix.Notify.failure(`Rejected promise ${position} is ${delay}ms`); 
+  }); 
+  delay += step; 
+} 
+form.reset(); 
+} 
+ 
+function createPromise(position, delay) { 
+  return new Promise((resolve, reject) => { 
+  const shouldResolve = Math.random() > 0.3; 
+  setTimeout(() => { 
+  if (shouldResolve) { 
+    resolve ({position, delay}); 
+  } else { 
+    reject({position, delay}) 
+  } 
+}, delay); 
+  }); 
 }
